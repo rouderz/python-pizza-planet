491830065aa5494c796a9818b349b6efd2c0dd2e
Wolfgang Welcomez <wolfgangwilfredo@gmail.com>
2023-01-29 10:26:49 -0500

    PIZ: (11) Create terraform to frontend

--- infrastructure/backend/main.tf
+++ infrastructure/backend/main.tf
@@ -253,3 +253,68 @@ resource "aws_db_instance" "db-instance-rds" {
   parameter_group_name   = aws_db_parameter_group.aws-parameter-group.name
   publicly_accessible    = true
 }
+
+
+#Domain
+
+resource "aws_route53_zone" "aws-pizza-zone" {
+  name = "wwelcomez.ioet.com."
+}
+
+resource "aws_route53_record" "aws-pizza-record" {
+  for_each = {
+    for dvo in aws_acm_certificate.aws_acm_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.aws-pizza-zone.zone_id
+}
+
+resource "aws_route53_record" "aws-pizza-main_record" {
+  zone_id = aws_route53_zone.aws-pizza-zone.zone_id
+  name    = "api.pizza.wwelcomez.ioet.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_api_gateway_domain_name.aws-api-domain-name.cloudfront_domain_name
+    zone_id                = aws_api_gateway_domain_name.aws-api-domain-name.cloudfront_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_acm_certificate_validation" "acm_cert_validation" {
+  depends_on = [
+    aws_route53_zone.aws-pizza-zone,
+    aws_route53_record.aws-pizza-record
+  ]
+  certificate_arn         = aws_acm_certificate.aws_acm_cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.aws-pizza-record : record.fqdn]
+}
+
+resource "aws_acm_certificate" "aws_acm_cert" {
+  domain_name       = "api.pizza.wwelcomez.ioet.com"
+  validation_method = "DNS"
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_api_gateway_base_path_mapping" "aws-api-gw-domain-mapping" {
+  api_id      = aws_api_gateway_rest_api.api-gateway-rest-api.id
+  stage_name  = aws_api_gateway_deployment.api-gateway-deployment.stage_name
+  domain_name = aws_api_gateway_domain_name.aws-api-domain-name.domain_name
+}
+
+resource "aws_api_gateway_domain_name" "aws-api-domain-name" {
+  depends_on      = [aws_acm_certificate_validation.acm_cert_validation]
+  domain_name     = aws_acm_certificate.aws_acm_cert.domain_name
+  certificate_arn = aws_acm_certificate_validation.acm_cert_validation.certificate_arn
+}
